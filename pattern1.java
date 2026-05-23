472fd94670d580616a26c0362a5a7af3cd071fa2
yashwanth gorle <yashwanth.gorle117@gmail.com>
2026-05-23 22:11:41 +0530

    pattern 1 is added

---
+++ pattern1.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class pattern1 {
+
+    public static void main(String[] args) {
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n : ");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        sc.close();
+
+    }
+}
